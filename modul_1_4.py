f81f9ac46db864aa2d96d1854c6c77b04b502820
svetlanochkaa <aleksandrova6913@icloud.com>
2024-11-24 15:42:14 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,13 @@
+#1 Создайте модуль и напишите весь код в нем
+#modul_1_4.py
+
+#2 Организуйте программу
+my_string = input("Введите строку: ")
+print("Количество символов в строке:",len(my_string))
+
+#3 Работа с методами строк
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
